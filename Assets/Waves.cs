12d5627312ee7ab9e89167b4ffe5cfeac9a4e6ef
Stewart G <grant-stewart@hotmail.com>
2023-02-04 21:08:09 +1100

    updated room

--- Assets/Waves.cs
+++ Assets/Waves.cs
@@ -8,6 +8,7 @@ public class Waves : MonoBehaviour
 {
     public List<EnemyWave> EnemyWaveList = new List<EnemyWave>();
     Queue<EnemyType> Enemies;
+
     //Queue<GameObject> backup;
     int currentWave;
     public Text DebugText;
@@ -23,6 +24,7 @@ public class Waves : MonoBehaviour
     void SpawnWave(int currentWave)
     {
         Enemies = new Queue<EnemyType>();
+
         for (int i = 0; i < EnemyWaveList[currentWave].waveSize; i++)
         {
             GameObject Enemy = Instantiate(EnemyWaveList[currentWave].enemyList[Random.Range(0, EnemyWaveList[currentWave].enemyList.Length)], new Vector3(Random.Range(25, -25), Random.Range(15, -15), 0), Quaternion.identity);
@@ -50,19 +52,19 @@ public class Waves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Enemies.Count > 0)
+        if (Enemies.Count > 0 )
         return;
 
         currentWave++;
         DebugText.text = "";
         SpawnWave(currentWave);
-        Debug.Log("ohai mark");
     }
 
 }
 [System.Serializable]
 public class EnemyWave
 {
+    bool RandomWaveFromPool;
     public GameObject[] enemyList;
     public int waveSize = 5;
 }
