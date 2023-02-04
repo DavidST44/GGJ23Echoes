54ec1602f3f415dbc600e0bf28f572e6266cc525
Stewart G <grant-stewart@hotmail.com>
2023-02-04 20:54:43 +1100

    Waves

--- Assets/Waves.cs
+++ Assets/Waves.cs
@@ -1,33 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental.FileFormat;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Waves : MonoBehaviour
 {
-
-    Queue<GameObject> Enemies;
-    Queue<GameObject> backup;
-    public GameObject[] enemyList;
-    public int waveSize = 5;
+    public List<EnemyWave> EnemyWaveList = new List<EnemyWave>();
+    Queue<EnemyType> Enemies;
+    //Queue<GameObject> backup;
+    int currentWave;
+    public Text DebugText;
 
     // Start is called before the first frame update
     void Start()
     {
-        Enemies = new Queue<GameObject>(); 
-        for(int i = 0; i < waveSize; i++)
+        SpawnWave(0);
+
+        //backup = new Queue<GameObject>(Enemies);
+    }
+
+    void SpawnWave(int currentWave)
+    {
+        Enemies = new Queue<EnemyType>();
+        for (int i = 0; i < EnemyWaveList[currentWave].waveSize; i++)
         {
-            GameObject Enemy = Instantiate(enemyList[Random.Range(0,enemyList.Length)],new Vector3(Random.Range(25,-25), Random.Range(15, -15), 0), Quaternion.identity);
-            Enemies.Enqueue(Enemy);
+            GameObject Enemy = Instantiate(EnemyWaveList[currentWave].enemyList[Random.Range(0, EnemyWaveList[currentWave].enemyList.Length)], new Vector3(Random.Range(25, -25), Random.Range(15, -15), 0), Quaternion.identity);
+            Enemies.Enqueue(Enemy.GetComponent<Enemy>().ColourName);
 
-            print("Enemy Spawned: " + Enemy.name);
+            print("Enemy Spawned: " + Enemy.GetComponent<Enemy>().ColourName);
+            DebugText.text = DebugText.text + " " + Enemy.GetComponent<Enemy>().ColourName;
         }
-
-        backup = new Queue<GameObject>(Enemies);
     }
 
     public bool IsCorrectTarget(GameObject enemy)
     {
-        if (Enemies.Peek() == enemy)
+        if (Enemies.Peek() == enemy.GetComponent<Enemy>().ColourName)
         {
             Enemies.Dequeue();
             return true;
@@ -42,6 +50,19 @@ public class Waves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Enemies.Count > 0)
+        return;
+
+        currentWave++;
+        DebugText.text = "";
+        SpawnWave(currentWave);
+        Debug.Log("ohai mark");
     }
+
+}
+[System.Serializable]
+public class EnemyWave
+{
+    public GameObject[] enemyList;
+    public int waveSize = 5;
 }
